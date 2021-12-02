03fb11e23f9764535430403940b220dd824e1edc
Vaishnavi <vishisarda606@gmail.com>
2021-12-02 15:04:18 +0530

    New Commit

--- Js101/Js101_again/not_bad.js
+++
@@ -1,8 +0,0 @@
-// Create a function called notBad that takes a single argument, a string.
-// It should find the first appearance of the substring 'not' and 'bad'.
-// If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
-// If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
-
-function notBad(arg){
-    let not =sent .
-}
