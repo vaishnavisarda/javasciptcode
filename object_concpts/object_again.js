b0414639eb17db68b39219dccdcbc789a2a2311d
Vaishnavi <vishisarda606@gmail.com>
2021-12-17 16:38:34 +0530

    New Commit

--- object_concpts/object_again.js
+++ object_concpts/object_again.js
@@ -1,39 +1,79 @@
-// function Person(first,last,age,gender,interests){
-//     this.name ={
-//         first,
-//         last
-//     };
-//    this.age=age
-//     this.gender=gender
-//    this.interests =interests;
-//    console.log(age)
-// // };
-//    function Brick(){
-//        this.width =10;
-//        this.height =20;
-//    }
+// // function Person(first,last,age,gender,interests){
+// //     this.name ={
+// //         first,
+// //         last
+// //     };
+// //    this.age=age
+// //     this.gender=gender
+// //    this.interests =interests;
+// //    console.log(age)
+// // // };
+// //    function Brick(){
+// //        this.width =10;
+// //        this.height =20;
+// //    }
 
-//    function BlueGlassBrick(){
-//        Brick.call(this);
+// //    function BlueGlassBrick(){
+// //        Brick.call(this);
 
-//        this.opacity =0.5;
-//        this.color = "blue";
-//    }
+// //        this.opacity =0.5;
+// //        this.color = "blue";
+// //    }
 
-// const person ={};
-// console.log(person);
+// // const person ={};
+// // console.log(person);
 
 
-const person={
-    name:['Vaishnavi','Gauri'],
-    age :23,
-    gender : 'female',
-    interestd : ['music','sketing'],
+// const person={
+//     name:['Vaishnavi','Gauri'],
+//     age :23,
+//     gender : 'female',
+//     interestd : ['music','sketing'],
 
-    bio: function(){
-        console.log(this.name[0]+' '+ this.name[1]+'is' + this.age + 'years old.He likes' + this.interests[0]+ 'and' +  this.interests[1]+ '.'); 
-    },
-    greeting : function(){
-        console.log('Hi ! I\'m' + this.name[0] + '.');
-    }
-};
+//     bio: function(){
+//         console.log(this.name[0]+' '+ this.name[1]+'is' + this.age + 'years old.He likes' + this.interests[0]+ 'and' +  this.interests[1]+ '.'); 
+//     },
+//     greeting : function(){
+//         console.log('Hi ! I\'m' + this.name[0] + '.');
+//     }
+// };
+
+
+let cat = {
+    name: 'Vaishnavi',
+    color: 'white',
+    greeting: defGreeting,
+    color: 'black'
+}
+
+function defGreeting() {
+    console.log('Hi I am  greeting1 ' + this.name + '.' + this.name1);
+    return 1;
+}
+
+// Put your code here
+console.log(cat.name);
+
+
+console.log(cat.color);
+
+console.log(cat.greeting);
+cat.greeting()
+//this = {}
+console.log(defGreeting());
+//this = cat
+console.log(cat.greeting());
+console.log(defGreeting.call(cat));
+//this = {
+// name : "name"
+//}
+console.log(defGreeting.call({
+    name: "Sumit"
+}));
+
+console.log(cat.greeting.call({
+    name: "name"
+}));
+
+
+
