c78130df09a9cb2e4e1173af6f4c6e0a8c225eaf
Vaishnavi <vishisarda606@gmail.com>
2021-12-17 17:00:10 +0530

    New Commit

---
+++ object_concpts/object_band.js
@@ -0,0 +1,25 @@
+// Include at least two albums in the albums array.
+
+// Once you've done this, you should then write a string to the variable bandInfo, which will contain a small biography detailing their name, nationality, years active, and style, and the title and release date of their first album.
+
+let bandInfo ={
+
+    name : "Vaishnavi",
+    nationality :"Indian",
+    genre:"Indian music",
+    members : 2,
+    formed :  2018,
+    split :false,
+    albums :[{
+        name :"Sooryavansham",
+        released : 1998
+    },
+    {
+        name: "Welcome",
+        released : 2001
+    }]
+    console.log('Hi we are' + bandInfo.name );
+    }
+
+    
+    
